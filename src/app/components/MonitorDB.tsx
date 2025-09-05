509fd06bbbf6bdc6eae264e6293701d6b69eaaa3
Chris Tomotsugu <tomo@Chriss-MacBook-Pro.local>
2025-09-05 18:35:09 -0400

    created the email and db components

--- src/app/components/MonitorDB.tsx
+++ src/app/components/MonitorDB.tsx
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+// import gsap from "gsap";
+
+// const CircleButton = styled.button`
+//   font-family: var(--font-space-mono);
+//   font-weight: 700;
+//   color: black;
+//   font-size: 0.875rem;
+//   background-color: #b6bfc8;
+//   text-align: center;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 50%;
+//   width: 2.5rem;
+//   height: 2.5rem;
+//   cursor: pointer;
+// `;
+//
+const TITLE = styled.div`
+  font-family: var(--font-work-sans);
+  font-weight: 800;
+  color: black;
+  font-size: 2rem;
+  padding: 6rem 2rem 0.5rem 2rem;
+`;
+
+const SUBTITLE = styled.div`
+  font-family: var(--font-space-mono);
+  /*font-weight: 400;*/
+  color: black;
+  font-size: 0.875rem;
+  /*padding: 0.5rem 1rem 0 2rem;*/
+  padding: 0rem 0rem 0rem 2rem;
+`;
+
 const MonitorDB = () => {
-  return <div>monitor database component</div>;
+  return (
+    <div
+      className=" w-full h-[calc(100vh-96px)] rounded-t-4xl py-8"
+      style={{ backgroundImage: "var(--gradient)" }}
+    >
+      <TITLE>DATABASE</TITLE>
+      <SUBTITLE className="font-[700]">PUB DEV PIT II</SUBTITLE>
+      <SUBTITLE className="font-[400]">September 18, 2025</SUBTITLE>
+    </div>
+  );
 };
 
 export default MonitorDB;
--- src/app/components/WriteEmail.tsx
+++ src/app/components/WriteEmail.tsx
@@ -1,5 +1,44 @@
-const WriteEmail = () => {
-  return <div>write email component</div>;
+import styled from "styled-components";
+// import gsap from "gsap";
+
+const TITLE = styled.div`
+  font-family: var(--font-work-sans);
+  font-weight: 800;
+  color: black;
+  font-size: 2rem;
+  padding: 6rem 2rem 0.5rem 2rem;
+`;
+
+const SUBTITLE = styled.div`
+  font-family: var(--font-space-mono);
+  color: black;
+  font-size: 0.875rem;
+  padding: 0rem 0rem 0rem 2rem;
+`;
+
+const WriteEamil = () => {
+  return (
+    <div
+      className=" w-full h-[calc(100vh-96px)] rounded-t-4xl py-8"
+      style={{ backgroundImage: "var(--gradient)" }}
+    >
+      <TITLE>EMAIL CLIENT</TITLE>
+      <SUBTITLE className="font-[700]">PUB DEV PIT II</SUBTITLE>
+      <SUBTITLE className="font-[400]">September 18, 2025</SUBTITLE>
+      <div className="px-8 pt-10">
+        <div className="rounded-xl w-full px-2 py-2 bg-[var(--colordarkgrey)]">
+          <p className="[font-family:var(--font-work-sans)] font-[800] text-black">
+            MESSAGE
+          </p>
+        </div>
+      </div>
+      <div className="px-8 pt-4">
+        <div className="rounded-xl w-full h-[calc(100vh-30rem)] px-2 py-2 bg-[var(--colordarkgrey)]">
+          email sender
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default WriteEmail;
+export default WriteEamil;
